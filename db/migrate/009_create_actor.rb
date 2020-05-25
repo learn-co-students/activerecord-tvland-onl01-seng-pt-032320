7f7d9cd2268402763f09cff5781a5357f54e83f3
Curtis <laurencechadwell@gmail.com>
2020-05-24 22:12:47 -0500

    Done.

---
+++ db/migrate/009_create_actor.rb
@@ -0,0 +1,10 @@
+class CreateActor < ActiveRecord::Migration[5.1]
+    
+    def change
+      create_table :actors do |actor| 
+        actor.string :first_name
+        actor.string :last_name
+      end
+    end
+  end
+  
