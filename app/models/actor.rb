650824ec75d5bdf087125fb5630c55da4a30de4e
Vlad <VladDaImpaler@gmail.com>
2020-06-12 02:23:43 -0400

    finish migrations, progress on models

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,12 @@
 class Actor < ActiveRecord::Base
+  has_many :chracters
+  has_many :shows, through: :chracters
+
+  def full_name
+    [self.first_name, self.last_name].join(" ")
+  end
   
+  def list_roles
+    binding.pry
+  end
 end
