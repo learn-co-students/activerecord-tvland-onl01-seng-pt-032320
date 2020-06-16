3e981e4efe2a3a7e8093d5b6fdd2e6f8f0fda6c5
github username <github email address>
2020-06-16 16:28:53 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,6 +3,18 @@ class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
 
+  def full_name
+    #binding.pry
+    "#{self.first_name + " " + self.last_name}"
+  end
+
+  def list_roles
+     #binding.pry
+    self.characters.map do |character|
+      "#{character.name} - #{character.show.name}"
+    end
+  end   
+#we need to iterate over all the items in the array bc we can have arrays with alot of characters in it for the actor
 end
 
 
