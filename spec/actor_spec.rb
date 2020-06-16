cfc4b43e3cc7aa1d4eb216696349986111d63549
github username <github email address>
2020-06-16 00:46:56 -0400

    Done.

--- spec/actor_spec.rb
+++ spec/actor_spec.rb
@@ -36,7 +36,7 @@ describe "Actor" do
     khaleesi = Character.new(:name => "Khaleesi")
     khaleesi.actor = emilia
     got = Show.new(:name => "Game of Thrones")
-    khaleesi.show = got
+    khaleesi.show = got 
     khaleesi.save
 
     khaleesi.reload
