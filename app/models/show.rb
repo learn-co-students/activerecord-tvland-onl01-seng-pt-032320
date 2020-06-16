9b87c8de1a381aa73bcb045d07b17cae837fa6fc
github username <github email address>
2020-06-16 17:34:59 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -10,5 +10,14 @@ class Show < ActiveRecord::Base
     belongs_to :network
     has_many :characters
     has_many :actors, :through => :characters
+
+    def actors_list
+        self.actors.map do |actor|
+            #gives us list of actor instances, map thru each actor instances and returning back full_names (not instance itself)
+            #we havec array of all actor instances, we want to return name in array list
+            #map = iterates thru array
+            actor.full_name
+        end
+    end
   
 end
