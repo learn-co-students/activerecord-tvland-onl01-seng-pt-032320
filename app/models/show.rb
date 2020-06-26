281b4e68ccc126fe1b974b7790df500e80ca8ad6
arnie b <arnold.bwaila@gmail.com>
2020-06-26 14:17:04 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
+  def actors_list
+    self.characters.collect do |c|
+      c.actor.full_name
+    end
+  end
 end
